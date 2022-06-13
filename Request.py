85f3b8df620a2b66ee0ea6a182a23d9045ea919e
diogomgsimoes <dmg.simoes@campus.fct.unl.pt>
2022-06-14 00:02:06 +0100

    Simple input form

---
+++ Request.py
@@ -0,0 +1,6 @@
+class Request:
+    def __init__(self, location):
+        self.location = location
+
+    def test(self):
+        print(self.location)
--- app.py
+++ app.py
@@ -1,7 +1,30 @@
-from flask import Flask
+from flask import Flask, request
+from Request import Request
 
 app = Flask(__name__)
 
+@app.route('/', methods=["POST"])
+def read_post():
+    _request = Request(request.form["destination"])
+    _request.test()
+    return request.form["destination"]
+
 @app.route('/')
-def hello():
-    return '<h1>Hello, World! Pogchampion! Thuglife.</h1>'
+def init():
+    return """<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Plan your trip</title>
+</head>
+
+<body>
+<h1>Plan your trip</h1>
+<form method="POST">
+    <input name="destination">
+    <input type="submit">
+</form>
+
+</body>
+</html>"""
